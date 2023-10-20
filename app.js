f8ddacc5a7b184543fc20a78ecd2c67cac283193
jenien <riska.jeen@gmail.com>
2023-10-20 13:48:45 +0700

    baru sampe npm install

--- app.js
+++ app.js
@@ -1,64 +1,41 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const { PORT = 3000, JWT_SECRET } = process.env;
-const nasabahRoutes = require('./routes/nasabah.routes');
-const akunBankRoutes = require('./routes/akun_bank.routes');
-const transaksiRoutes = require('./routes/transaksi.routes');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) {
-        return res.status(403).json({
-            status: false,
-            message: 'Token tidak disediakan',
-            data: null
-        });
-    }
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-    jwt.verify(token, JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({
-                status: false,
-                message: 'Token tidak valid',
-                data: null
-            });
-        }
+var app = express();
 
-        req.user = decoded;
-        next();
-    });
-};
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(cors());
-app.use(express.json());
 app.use(logger('dev'));
-app.use('/nasabah', nasabahRoutes);
-app.use('/akun', verifyToken, akunBankRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    res.status(404).json({
-        status: false,
-        message: 'Tidak ditemukan xixixi',
-        data: null
-    });
-    next();
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).json({
-        status: false,
-        message: 'Data salah, coba yg bner y',
-        data: err.message
-    });
-    next();
-});
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(PORT, () => console.log('Jalan di port', PORT));
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
